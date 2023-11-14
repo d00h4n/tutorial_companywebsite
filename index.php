2695adf37ae9e224e9d5fae6cd506a3df73fcad7
d00h4n <doohan.mintarso@gmail.com>
2023-11-14 14:09:56 +0700

    Partner_update_database

--- index.php
+++ index.php
@@ -1,91 +1,81 @@
 <?php include_once("inc_header.php") ?>
-        <!-- untuk home -->
-        <section id="home">
-            <img src="<?php echo ambil_gambar(11) ?>" >
-            <div class="kolom">
-                <p class="deskripsi"><?php echo ambil_kutipan(11) ?> </p>
-                <h2><?php echo ambil_judul(11) ?></h2>
-                <?php echo maximum_kata(ambil_isi(11),20) ?>
-                <p><a href="<?php echo buat_link_halaman('11') ?>" class="tbl-pink">Pelajari Lebih Lanjut</a></p>
-            </div>
-        </section>
+<!-- untuk home -->
+<section id="home">
+    <img src="<?php echo ambil_gambar(11) ?>">
+    <div class="kolom">
+        <p class="deskripsi"><?php echo ambil_kutipan(11) ?> </p>
+        <h2><?php echo ambil_judul(11) ?></h2>
+        <?php echo maximum_kata(ambil_isi(11), 20) ?>
+        <p><a href="<?php echo buat_link_halaman('11') ?>" class="tbl-pink">Pelajari Lebih Lanjut</a></p>
+    </div>
+</section>
 
-        <!-- untuk courses -->
-        <section id="courses">
-            <div class="kolom">
-                <p class="deskripsi"><?php echo ambil_kutipan(10) ?>/p>
-                <h2><?php echo ambil_judul(10) ?></h2>
-                <?php echo maximum_kata(ambil_isi(10),25) ?>
-                <p><a href="<?php echo buat_link_halaman('10') ?>" class="tbl-biru">Pelajari Lebih Lanjut</a></p>
-            </div>
-            <img src="<?php echo ambil_gambar(10) ?>" />
-        </section>
+<!-- untuk courses -->
+<section id="courses">
+    <div class="kolom">
+        <p class="deskripsi"><?php echo ambil_kutipan(10) ?>/p>
+        <h2><?php echo ambil_judul(10) ?></h2>
+        <?php echo maximum_kata(ambil_isi(10), 25) ?>
+        <p><a href="<?php echo buat_link_halaman('10') ?>" class="tbl-biru">Pelajari Lebih Lanjut</a></p>
+    </div>
+    <img src="<?php echo ambil_gambar(10) ?>" />
+</section>
 
-        <!-- untuk tutors -->
-        <section id="tutors">
-            <div class="tengah">
-                <div class="kolom">
-                    <p class="deskripsi">Our Top Tutors</p>
-                    <h2>Tutors</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, optio!</p>
-                </div>
+<!-- untuk tutors -->
+<section id="tutors">
+    <div class="tengah">
+        <div class="kolom">
+            <p class="deskripsi">Our Top Tutors</p>
+            <h2>Tutors</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, optio!</p>
+        </div>
 
-                <div class="tutor-list">
-                    <?php 
-                    $sql1       = "select * from tutors order by id desc";
-                    $q1       = mysqli_query($koneksi,$sql1);
-                    while($r1 = mysqli_fetch_array($q1)){
-                    ?>
-                    <div class="kartu-tutor">
-                        <img src="<?php echo url_dasar()."/gambar/".tutors_foto($r1['id']) ?>" />
+        <div class="tutor-list">
+            <?php
+            $sql1       = "select * from tutors order by id desc";
+            $q1       = mysqli_query($koneksi, $sql1);
+            while ($r1 = mysqli_fetch_array($q1)) {
+            ?>
+                <div class="kartu-tutor">
+                    <a href="<?php echo buat_link_tutors($r1['id']) ?>">
+                        <img src="<?php echo url_dasar() . "/gambar/" . tutors_foto($r1['id']) ?>" />
                         <p><?php echo $r1['nama'] ?></p>
-                    </div>
-                        <?php
-                    }
-                    ?>
-                    
-                    <div class="kartu-tutor">
-                        <img src="https://images.ctfassets.net/1wryd5vd9xez/4DxzhQY7WFsbtTkoYntq23/a4a04701649e92a929010a6a860b66bf/https___cdn-images-1.medium.com_max_2000_1_Y6l_FDhxOI1AhjL56dHh8g.jpeg" />
-                        <p>John Doe</p>
-                    </div>
-                    <div class="kartu-tutor">
-                        <img src="https://images.fastcompany.net/image/upload/w_596,c_limit,q_auto:best,f_auto/fc/3021752-inline-i-1-why-square-designed-its-new-offices-to-work-like-a-city.jpg" />
-                        <p>Michael Dell</p>
-                    </div>
-                    <div class="kartu-tutor">
-                        <img src="https://blogs-images.forbes.com/jackkelly/files/2019/06/Jack-Kelly_avatar_1559658819-400x400.jpg" />
-                        <p>Bruce Wills</p>
-                    </div>
+                    </a>
                 </div>
-            </div>
-        </section>
+            <?php
+            }
+            ?>
+        </div>
+    </div>
+</section>
 
-        <!-- untuk partners -->
-        <section id="partners">
-            <div class="tengah">
-                <div class="kolom">
-                    <p class="deskripsi">Our Top Partners</p>
-                    <h2>Partners</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi magni tempore expedita sequi. Similique rerum doloremque impedit saepe atque maxime.</p>
+<!-- untuk partners -->
+<section id="partners">
+    <div class="tengah">
+        <div class="kolom">
+            <p class="deskripsi">Our Top Partners</p>
+            <h2>Partners</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi magni tempore expedita sequi. Similique rerum doloremque impedit saepe atque maxime.</p>
+        </div>
+
+        <div class="partner-list">
+            <?php
+
+            $sql1 = "select * from partners order by id asc";
+            $q1     = mysqli_query($koneksi, $sql1);
+            while ($r1 = mysqli_fetch_assoc($q1)) {
+            ?>
+                <div class="kartu-partner">
+                    <a href="<?php echo buat_link_partners($r1['id']) ?>">
+                        <img src="<?php echo url_dasar() . "/gambar/" . partners_foto($r1['id']) ?>">
+                    </a>
                 </div>
 
-                <div class="partner-list">
-                    <div class="kartu-partner">
-                        <img src="https://www.designevo.com/res/templates/thumb_small/black-wheat-and-mortarboard.png" />
-                    </div>
-                    <div class="kartu-partner">
-                        <img src="https://image.freepik.com/free-vector/campus-collage-university-education-logo-design-template_7492-63.jpg" />
-                    </div>
-                    <div class="kartu-partner">
-                        <img src="https://image.freepik.com/free-vector/campus-collage-university-education-logo-design-template_7492-62.jpg" />
-                    </div>
-                    <div class="kartu-partner">
-                        <img src="https://www.designevo.com/res/templates/thumb_small/encircled-branches-and-book.png" />
-                    </div>
-                    <div class="kartu-partner">
-                        <img src="https://image.freepik.com/free-vector/campus-collage-university-education-logo-design-template_7492-64.jpg" />
-                    </div>
-                </div>
-            </div>
-        </section>
-        <?php include_once("inc_footer.php") ?>
+            <?php
+            }
+
+            ?>
+        </div>
+    </div>
+</section>
+<?php include_once("inc_footer.php") ?>
